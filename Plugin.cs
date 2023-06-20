ffd83b067ae90e416cb67f9e49da823f1d86925f
Zeprus <zeprusaliastrylon@googlemail.com>
2023-06-20 13:31:25 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using System.IO;
+using System.Reflection;
+
+namespace SophisticatedMonsters
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("AssetReplacer")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            string modFolder = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)).Name;
+            AssetReplacer.API.Register(modFolder, AssetReplacer.AssetType.Textures);
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
